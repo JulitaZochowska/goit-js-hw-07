d25bfba32647a603c7ed26f38026a41ee9d82436
JulitaZochowska <julita.zochowska@outlook.com>
2023-04-25 12:43:04 +0200

    Exercise 1-finally fixed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,72 +3,40 @@ import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
 
-const link = document.querySelectorAll(".gallery__link");
+const galleryList = document.querySelector(".gallery__list");
+for (const galleryItem of galleryItems) {
+}
+
+const links = document.querySelectorAll(".gallery__link");
 //zablokuje domyślne zachowanie kliknięcia na linku
-link.addEventListener("click", (event) => {
-  event.preventDefault();
-});
+links.forEach((link) =>
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+  })
+);
+
 const gallery = document.querySelector(".gallery__item");
-gallery.addEventListener("click", (event) => {
-  if (event.target.tagName === "img") {
-    //sprawdza czy kliknięty element jest obrazem
-    const fullURL = event.target.dataset.source; //pobiera wartość atrybutu data-source, który
-    //zawiera URL do duzego obrazu.
-    window.open(fullURL, "_blank"); //otwiera większy obraz w nowej karcie
-  }
-});
-const gallery = document.querySelector(".gallery__item");
+// Pobieram elementy <img>
 const images = gallery.querySelectorAll(".gallery__image");
 
 images.forEach((image) => {
+  // Dodaję nasłuchiwanie na kliknięcie na <img>
   image.addEventListener("click", (event) => {
     event.preventDefault(); // zapobiega domyślnej akcji kliknięcia
 
+    // Utwórz nowy obiekt Lightbox z adresem URL nowego obrazu
     const imageUrl = image.getAttribute("href");
 
     const lightbox = basicLightbox.create(`<img src="${imageUrl}">`);
+    // Otwórz okno modalne z nowym obrazem
     lightbox.show();
   });
 });
 
-// Pobierz element <img>
-const img = document.querySelector(".gallery__image");
-
-// Dodaj nasłuchiwanie na kliknięcie na <img>
-img.addEventListener("click", () => {
-  // Utwórz nowy obiekt Lightbox z adresem URL nowego obrazu
-  const lightbox = basicLightbox.create(`<img src="nowy-obraz.jpg">`);
-
-  // Otwórz okno modalne z nowym obrazem
-  lightbox.show();
+// Dodajemy nasłuchiwanie na klawiaturę
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    // Zamknij okno modalne, jeśli naciśnięto Escape
+    lightbox.close();
+  }
 });
-<!-- Dodajemy znacznik dla obrazka, który będzie otwierał okno modalne -->
-<img id="myImage" src="myImage.jpg" alt="My Image">
-
-<!-- Właściwe okno modalne, ukrywane przez CSS -->
-<div id="myLightbox" style="display:none;">
-  <img src="" alt="">
-</div>
-
-<script>
-  const imageElement = document.getElementById('myImage');
-  const imageUrl = 'myImage.jpg';
-  
-  // Nasłuchiwanie na kliknięcie obrazka
-  imageElement.addEventListener('click', function() {
-    // Ustawiamy adres URL obrazka w oknie modalnym
-    const lightboxImage = document.querySelector('#myLightbox img');
-    lightboxImage.setAttribute('src', imageUrl);
-
-    // Otwieramy okno modalne
-    basicLightbox.create(document.querySelector('#myLightbox')).show();
-
-    // Dodajemy nasłuchiwanie na klawiaturę
-    document.addEventListener('keydown', function(e) {
-      if (e.key === 'Escape') {
-        // Zamknij okno modalne, jeśli naciśnięto Escape
-        basicLightbox.close();
-      }
-    });
-  });
-</script>
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const images = gallery.querySelector("gallery__images");
+
+images.forEach((image) => {
+  image.addEventListener("click", (event) => {
+    event.preventDefault(); // zapobiega domyślnej akcji kliknięcia
+    let gallery = new SimpleLightbox(".gallery a", {
+      captionsData: "alt",
+      captionDelay: "250",
+    });
+  });
+  gallery.on("show.simplelightbox", function () {
+    // do something…
+  });
+
+  gallery.on("error.simplelightbox", function (e) {
+    console.log(e); // some usefull information
+  });
+});
