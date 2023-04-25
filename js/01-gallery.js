4c07df582b4f46edfe1353504c71451c52023cc1
JulitaZochowska <julita.zochowska@outlook.com>
2023-04-25 14:13:37 +0200

    Exercise 2 fixed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,37 +3,65 @@ import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
 
-const galleryList = document.querySelector(".gallery__list");
+const galleryList = document.querySelector(".gallery");
+
+//tworzymy tylko JEDEN obiekt lightbox, bo jest drogi :)
+//tworzymy na początku, aby był dostępny globalnie
+//metoda do tworzenia lightboxa z internetu
+const lightbox = basicLightbox.create("");
+
+//Uwaga!Nie dodajesz juz niczego ręcznie do HTML!!!
+//Tworzymy naszego HTML
 for (const galleryItem of galleryItems) {
-}
+  //tworzymy znacznik <li>
+  const listItem = document.createElement("li");
+  //tworzymy znacznik <div>
+  const div = document.createElement("div");
+  //dodajemy klasę do taga <div>
+  div.classList.add("gallery__item");
+  //tworzymy znacznik <a>
+  const a = document.createElement("a");
+  //dodajemy klasę do znacznika <a>
+  a.classList.add("gallery__link");
+  //przypisujemy ściekę do taga <a> (pochodzi z gallery-items.js)
+  a.href = galleryItem.original;
 
-const links = document.querySelectorAll(".gallery__link");
-//zablokuje domyślne zachowanie kliknięcia na linku
-links.forEach((link) =>
-  link.addEventListener("click", (event) => {
+  // dodajemy event listener tutaj, bo nie ma sensu brac go z HTML za pomoca document.getElement/querySelector
+  // skoro mamy do niego dostep tutaj. Operacja getElement/querySelector jest "droga"
+  // i jezeli mozna, powinno sie jej unikac
+  //addEventListener ustawiamy zawsze na pojedynczym elemencie!!!
+  //wszystkie operacje robimy na <a> a nie na <img> mimo, ze <img> tez zawiera sciezke w data-source!!!, bo zawsze
+  //klikamy w link
+  a.addEventListener("click", (event) => {
+    //zablokuje domyślne zachowanie kliknięcia na linku
     event.preventDefault();
-  })
-);
 
-const gallery = document.querySelector(".gallery__item");
-// Pobieram elementy <img>
-const images = gallery.querySelectorAll(".gallery__image");
-
-images.forEach((image) => {
-  // Dodaję nasłuchiwanie na kliknięcie na <img>
-  image.addEventListener("click", (event) => {
-    event.preventDefault(); // zapobiega domyślnej akcji kliknięcia
-
-    // Utwórz nowy obiekt Lightbox z adresem URL nowego obrazu
-    const imageUrl = image.getAttribute("href");
-
-    const lightbox = basicLightbox.create(`<img src="${imageUrl}">`);
-    // Otwórz okno modalne z nowym obrazem
+    // ustawiamy jedynie "treść" lightboxa (funcja element() zwraca uchwyt do kontenera okna modalnego)
+    // a po ustawieniu treści - duzego obrazka - wyświetlamy
+    // jakby uzyc metody create() do lightboxa tutaj, to tworzylibysmy nowy obiekt lightbox za kazdym razem
+    // jak klikniemy, co mogloby zapchac pamiec - tzw. memory leak
+    lightbox.element().innerHTML = `<img src="${galleryItem.original}" alt="${galleryItem.description}" />`;
+    //wyświetlamy okno modalne
     lightbox.show();
   });
-});
 
-// Dodajemy nasłuchiwanie na klawiaturę
+  const img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.src = galleryItem.preview;
+  img.dataset.source = galleryItem.original;
+  img.alt = galleryItem.description;
+
+  //dodajemy <img> jako dziecko <a>
+  a.appendChild(img);
+  //dodajemy <a> jako dziecko <div>
+  div.appendChild(a);
+  //dodajemy <div> jako dziecko <li>
+  listItem.appendChild(div);
+  //dodajemy <li> jako dziecko <ul>
+  galleryList.appendChild(listItem);
+}
+
+// Dodajemy nasłuchiwanie na klawiaturę, zamknięcie z klawiatury
 document.addEventListener("keydown", (event) => {
   if (event.key === "Escape") {
     // Zamknij okno modalne, jeśli naciśnięto Escape
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,24 +1,36 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+//SimpleLightbox-biblioteka do tworzenia okien modalnych, czyli masz galerię obrazów i
+//zeby kazdy obrazek wyskakiwal pojedynczo powiekszony
 
 console.log(galleryItems);
 
-const gallery = document.querySelector(".gallery");
-const images = gallery.querySelector("gallery__images");
+const galleryList = document.querySelector(".gallery");
+//musisz to robić w pętli i iterować po kazdym elemencie tablicy a nie odrazu wrzucac cala ogromna tablice
+//zamiast tej pętli mozna uzyć metody forEach
+for (const galleryItem of galleryItems) {
+  const listItem = document.createElement("li");
 
-images.forEach((image) => {
-  image.addEventListener("click", (event) => {
-    event.preventDefault(); // zapobiega domyślnej akcji kliknięcia
-    let gallery = new SimpleLightbox(".gallery a", {
-      captionsData: "alt",
-      captionDelay: "250",
-    });
-  });
-  gallery.on("show.simplelightbox", function () {
-    // do something…
-  });
+  const a = document.createElement("a");
+  a.classList.add("gallery__item");
+  a.href = galleryItem.preview;
 
-  gallery.on("error.simplelightbox", function (e) {
-    console.log(e); // some usefull information
-  });
+  const img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.src = galleryItem.preview;
+  img.alt = galleryItem.description;
+
+  a.appendChild(img);
+  listItem.appendChild(a);
+  galleryList.appendChild(listItem);
+}
+//(.gallery a, a jest potomkiem elementu o klasie gallery
+//tworzenie okna modalnego
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250",
+});
+//sprawdzenie, czy jest jakiś błąd
+gallery.on("error.simplelightbox", function (e) {
+  console.log(e); // some usefull information
 });
